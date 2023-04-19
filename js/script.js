59b098181ddce70d9d75bdbd0b43ac181b8ef52f
João Luciano <104032132+JoaoLuciano1429@users.noreply.github.com>
2023-04-19 19:18:48 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,99 @@
+document.getElementById("AMG").addEventListener("click", function (ev) {
+  ev = document.getElementById("AmericaMG");
+  ev.play();
+});
+
+document.getElementById("CAP").addEventListener("click", function (ev) {
+  ev = document.getElementById("AthleticoPR");
+  ev.play();
+});
+
+document.getElementById("CAM").addEventListener("click", function (ev) {
+  ev = document.getElementById("AtleticoMG");
+  ev.play();
+});
+
+document.getElementById("BAH").addEventListener("click", function (ev) {
+  ev = document.getElementById("Bahia");
+  ev.play();
+});
+
+document.getElementById("BOT").addEventListener("click", function (ev) {
+  ev = document.getElementById("Botafogo");
+  ev.play();
+});
+
+document.getElementById("COR").addEventListener("click", function (ev) {
+  ev = document.getElementById("Corinthians");
+  ev.play();
+});
+
+document.getElementById("CFC").addEventListener("click", function (ev) {
+  ev = document.getElementById("Coritiba");
+  ev.play();
+});
+
+document.getElementById("CRU").addEventListener("click", function (ev) {
+  ev = document.getElementById("Cruzeiro");
+  ev.play();
+});
+
+document.getElementById("CUI").addEventListener("click", function (ev) {
+  ev = document.getElementById("Cuiaba");
+  ev.play();
+});
+
+document.getElementById("FLA").addEventListener("click", function (ev) {
+  ev = document.getElementById("Flamengo");
+  ev.play();
+});
+
+document.getElementById("FLU").addEventListener("click", function (ev) {
+  ev = document.getElementById("Fluminense");
+  ev.play();
+});
+
+document.getElementById("FOR").addEventListener("click", function (ev) {
+  ev = document.getElementById("Fortaleza");
+  ev.play();
+});
+
+document.getElementById("GOI").addEventListener("click", function (ev) {
+  ev = document.getElementById("Goias");
+  ev.play();
+});
+
+document.getElementById("GRE").addEventListener("click", function (ev) {
+  ev = document.getElementById("Gremio");
+  ev.play();
+});
+
+document.getElementById("INT").addEventListener("click", function (ev) {
+  ev = document.getElementById("Internacional");
+  ev.play();
+});
+
+document.getElementById("PAL").addEventListener("click", function (ev) {
+  ev = document.getElementById("Palmeiras");
+  ev.play();
+});
+
+document.getElementById("RBB").addEventListener("click", function (ev) {
+  ev = document.getElementById("RbBragantino");
+  ev.play();
+});
+
+document.getElementById("SAN").addEventListener("click", function (ev) {
+  ev = document.getElementById("Santos");
+  ev.play();
+});
+
+document.getElementById("SAO").addEventListener("click", function (ev) {
+  ev = document.getElementById("SaoPaulo");
+  ev.play();
+});
+
+document.getElementById("VAS").addEventListener("click", function (ev) {
+  ev = document.getElementById("Vasco");
+  ev.play();
+});
